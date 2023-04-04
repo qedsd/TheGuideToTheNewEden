3fc3244bbaeebc19f8c726f45ef55a02dda19098
QEDSD <2998739882@qq.com>
2023-04-04 11:13:58 +0800

    添加游戏进程模拟工具

---
+++ TheGuideToTheNewEden.ExefileSimulation/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Hello, NewEden!");
+Console.Write("Input character name or full title name:");
+string input = Console.ReadLine();
+if(input.Contains('-'))
+{
+    Console.Title = input;
+}
+else
+{
+    Console.Title = $"EVE - {input}";
+}
+while (true)
+{
+    Console.ReadLine();
+}
