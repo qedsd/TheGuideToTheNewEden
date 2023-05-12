5ebab897315fa4de05f9a676f25b03877197ed06
QEDSD <2998739882@qq.com>
2023-05-12 17:50:59 +0800

    邮件

--- TheGuideToTheNewEden/TheGuideToTheNewEden.WinUI/TheGuideToTheNewEden.WinUI/Views/Character/MailPage.xaml.cs
+++ TheGuideToTheNewEden/TheGuideToTheNewEden.WinUI/TheGuideToTheNewEden.WinUI/Views/Character/MailPage.xaml.cs
@@ -1,6 +1,4 @@
-// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
-
+using ESI.NET;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -16,19 +14,41 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace TheGuideToTheNewEden.WinUI.Views.Character
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MailPage : Page
+    public sealed partial class MailPage : Page,ICharacterPage
     {
+        private BaseWindow _window;
+        private EsiClient _esiClient;
         public MailPage()
         {
             this.InitializeComponent();
+            Loaded += MailPage_Loaded;
+        }
+
+        private void MailPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            _window = Helpers.WindowHelper.GetWindowForElement(this) as BaseWindow;
+            if (!_isLoaded)
+            {
+                Refresh();
+                _isLoaded = true;
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            _esiClient = e.Parameter as EsiClient;
+        }
+        private bool _isLoaded = false;
+        public void Clear()
+        {
+            
+        }
+
+        public void Refresh()
+        {
+            
         }
     }
 }
