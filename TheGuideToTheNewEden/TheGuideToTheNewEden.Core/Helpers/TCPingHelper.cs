51c129c16c71fa622d200640640f768b5e942b06
QEDSD <2998739882@qq.com>
2023-07-04 15:09:45 +0800

    延迟UI美化

---
+++ TheGuideToTheNewEden/TheGuideToTheNewEden.Core/Helpers/TCPingHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGuideToTheNewEden.Core.Helpers
+{
+    public static class TCPingHelper
+    {
+        private static Stopwatch _stopwatch;
+        public static async Task<double> Ping(string host, int port)
+        {
+            if(_stopwatch == null)
+            {
+                _stopwatch = new Stopwatch();
+            }
+            _stopwatch.Restart();
+            var sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.ReceiveTimeout = 5000;
+            sock.Blocking = true;
+            try
+            {
+                await sock.ConnectAsync(host, port);
+                _stopwatch.Stop();
+                sock.Disconnect(false);
+                sock.Dispose();
+                return Math.Round(_stopwatch.Elapsed.TotalMilliseconds, 0);
+            }
+            catch (Exception ex)
+            {
+                Core.Log.Error(ex);
+                _stopwatch.Stop();
+                sock.Dispose();
+                return 0;
+            }
+        }
+    }
+}
--- TheGuideToTheNewEden/TheGuideToTheNewEden.WinUI/TheGuideToTheNewEden.WinUI/Views/ServerPingPage.xaml.cs
+++ TheGuideToTheNewEden/TheGuideToTheNewEden.WinUI/TheGuideToTheNewEden.WinUI/Views/ServerPingPage.xaml.cs
@@ -15,17 +15,36 @@ using Microsoft.UI.Xaml.Navigation;
 using System.Diagnostics;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using TheGuideToTheNewEden.Core.Helpers;
+using System.Net.NetworkInformation;
+using Syncfusion.UI.Xaml.Data;
+using ESI.NET.Models.Universe;
+using Newtonsoft.Json;
 
 namespace TheGuideToTheNewEden.WinUI.Views
 {
     public sealed partial class ServerPingPage : Page
     {
-        private Stopwatch _stopwatch;
         private BaseWindow _window;
+        private ObservableCollection<PingStatus> _pings = new ObservableCollection<PingStatus>();
+        private PingConfig _pingConfig;
         public ServerPingPage()
         {
             this.InitializeComponent();
+            _pingConfig = PingConfig.Load();
+            ComboBox_Host.Text = _pingConfig.Host;
+            NumberBox_Port.Value = _pingConfig.Port;
+            NumberBox_Times.Value = _pingConfig.Times;
+            NumberBox_Span.Value = _pingConfig.Span;
+            LineSeries1.ItemsSource = _pings;
             Loaded += ServerPingPage_Loaded;
+            Unloaded += ServerPingPage_Unloaded;
+        }
+
+        private void ServerPingPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Stop();
         }
 
         private void ServerPingPage_Loaded(object sender, RoutedEventArgs e)
@@ -35,57 +54,67 @@ namespace TheGuideToTheNewEden.WinUI.Views
 
         private void Button_Start_Click(object sender, RoutedEventArgs e)
         {
-            if(_stopwatch == null)
-            {
-                _stopwatch = new Stopwatch();
-            }
             if (_isStop)//准备开始
             {
-                
                 Start();
             }
             else
             {
-               
+
                 Stop();
             }
         }
         private void Start()
         {
-            if(string.IsNullOrEmpty(ComboBox_Host.Text))
+            if (string.IsNullOrEmpty(ComboBox_Host.Text))
             {
                 _window?.ShowError("请输入主机");
                 return;
             }
+            _pingConfig.Host = ComboBox_Host.Text;
+            _pingConfig.Port = (int)NumberBox_Port.Value;
+            _pingConfig.Times = (int)NumberBox_Times.Value;
+            _pingConfig.Span = (int)NumberBox_Span.Value;
+            _pingConfig.Save();
             Button_Start.Content = "停止";
+            _pings.Clear();
             Ping(ComboBox_Host.Text, (int)NumberBox_Port.Value, (int)NumberBox_Times.Value, NumberBox_Span.Value);
         }
         private void Stop()
         {
             Button_Start.Content = "开始";
             _isStop = true;
-            _stopwatch.Stop();
         }
         private bool _isStop = true;
         private async void Ping(string host, int port, int times, double span)
         {
-            TextBlock_Result.Text = string.Empty;
             _isStop = false;
-            for (int i = 0; i< times;i++)
+            for (int i = 0; i < times; i++)
             {
-                _stopwatch.Restart();
-                var sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                sock.Blocking = true;
-                await sock.ConnectAsync(host, port);
-                _stopwatch.Stop();
-                sock.Disconnect(false);
-                TextBlock_Result.Text += "连接至 " + host + ":" + port + " 延迟=" + Math.Round(_stopwatch.Elapsed.TotalMilliseconds, 0) + "ms\n";
-                if(_isStop)
+                var result = await TCPingHelper.Ping(host, port);
+                if (_isStop)
                 {
                     return;
                 }
                 else
                 {
+                    _pings.Add(new PingStatus(_pings.Count + 1, result));
+                    var validPings = _pings.Where(p => p.Ms > 0).ToList();
+                    if (validPings.Any())
+                    {
+                        var average = validPings.Average(p => p.Ms);
+                        TextBlock_RTTAverage.Text = average.ToString("F2");
+                        double sumP = 0;
+                        validPings.ForEach(p => sumP += Math.Abs(p.Ms - average) / average);
+                        TextBlock_RTTFluctuation.Text = (sumP / validPings.Count * 100).ToString("F2");
+                        TextBlock_PacketLossRate.Text = ((_pings.Count - validPings.Count) / _pings.Count * 100).ToString("F2");
+                    }
+                    else
+                    {
+                        TextBlock_RTTAverage.Text = "--";
+                        TextBlock_RTTFluctuation.Text = "--";
+                        TextBlock_PacketLossRate.Text = "100";
+                    }
                     await Task.Delay((int)span);
                 }
                 if (_isStop)
@@ -94,6 +123,47 @@ namespace TheGuideToTheNewEden.WinUI.Views
                 }
             }
             Stop();
+            _window?.ShowSuccess("已完成");
+        }
+
+        internal class PingStatus
+        {
+            internal PingStatus(int id, double ms)
+            {
+                Id = id;
+                Ms = ms;
+            }
+            public int Id { get; set; }
+            public double Ms { get; set; }
+        }
+
+        internal class PingConfig
+        {
+            public string Host { get; set; } = "tranquility.servers.eveonline.com";
+            public int Port { get; set; } = 26000;
+            public int Times { get; set; } = 8;
+            public int Span { get; set; } = 1500;
+            private static readonly string FilePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Configs", "PingConfig.json");
+            public static PingConfig Load()
+            {
+                if(System.IO.File.Exists(FilePath))
+                {
+                    var json = File.ReadAllText(FilePath);
+                    return JsonConvert.DeserializeObject<PingConfig>(json);
+                }
+                else
+                {
+                    return new PingConfig();
+                }
+            }
+            public void Save()
+            {
+                if(!Directory.Exists(Path.GetDirectoryName(FilePath)))
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(FilePath));
+                }
+                File.WriteAllText(FilePath, JsonConvert.SerializeObject(this));
+            }
         }
     }
 }
