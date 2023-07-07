6b434674905f49ea1b18cef9a5d033e2441dea81
QEDSD <2998739882@qq.com>
2023-07-07 17:04:58 +0800

    离线监测封存

---
+++ TheGuideToTheNewEden/TheGuideToTheNewEden.Core/Models/OfflineMonitorSetting.cs
@@ -0,0 +1,49 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheGuideToTheNewEden.Core.Models
+{
+    public class OfflineMonitorSetting : ObservableObject
+    {
+        public List<int> MonitoringCharacterIds { get; set; } = new List<int>();
+        private bool systemNotify = true;
+        /// <summary>
+        /// 系统消息通知
+        /// </summary>
+        public bool SystemNotify
+        {
+            get => systemNotify; set => SetProperty(ref systemNotify, value);
+        }
+
+        private bool mailNotify = false;
+        /// <summary>
+        /// 邮件通知
+        /// </summary>
+        public bool MailNotify
+        {
+            get => mailNotify; set => SetProperty(ref mailNotify, value);
+        }
+
+        private string fromMailAddr;
+        public string FromMailAddr { get => fromMailAddr; set => SetProperty(ref fromMailAddr, value); }
+
+        private string fromName;
+        public string FromName { get => fromName; set => SetProperty(ref fromName, value); }
+
+        private string fromPassword;
+        public string FromPassword { get => fromPassword; set => SetProperty(ref fromPassword, value); }
+
+        private string smtpHost = "smtp.qq.com";
+        public string SmtpHost { get => smtpHost; set => SetProperty(ref smtpHost, value); }
+
+        private int port = 587;
+        public int Port { get => port; set => SetProperty(ref port, value); }
+
+        private string toMailAddr;
+        public string ToMailAddr { get => toMailAddr; set => SetProperty(ref toMailAddr, value); }
+
+
+    }
+}
---
+++ TheGuideToTheNewEden/TheGuideToTheNewEden.WinUI/TheGuideToTheNewEden.WinUI/ViewModels/OfflineMonitorViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ESI.NET.Models.SSO;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGuideToTheNewEden.Core.Models;
+
+namespace TheGuideToTheNewEden.WinUI.ViewModels
+{
+    public class OfflineMonitorViewModel:BaseViewModel
+    {
+        public ObservableCollection<AuthorizedCharacterData> Characters { get; set; } = Services.CharacterService.CharacterOauths;
+        public ObservableCollection<AuthorizedCharacterData> SelectedCharacters = new ObservableCollection<AuthorizedCharacterData>();
+        private bool isRunning;
+        /// <summary>
+        /// 是否监控中
+        /// </summary>
+        public bool IsRunning
+        {
+            get => isRunning;
+            set => SetProperty(ref isRunning, value);
+        }
+
+        private OfflineMonitorSetting setting;
+        public OfflineMonitorSetting Setting
+        {
+            get => setting;
+            set => SetProperty(ref setting, value);
+        }
+    }
+}
