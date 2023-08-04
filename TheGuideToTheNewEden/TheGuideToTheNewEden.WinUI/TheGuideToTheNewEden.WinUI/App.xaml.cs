c81e4ee4329addc5b8b0290935bcf7ea5758c467
QEDSD <2998739882@qq.com>
2023-08-04 15:49:06 +0800

    unpackage初步改造

--- TheGuideToTheNewEden/TheGuideToTheNewEden.WinUI/TheGuideToTheNewEden.WinUI/App.xaml.cs
+++ TheGuideToTheNewEden/TheGuideToTheNewEden.WinUI/TheGuideToTheNewEden.WinUI/App.xaml.cs
@@ -1,26 +1,30 @@
 ﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using TheGuideToTheNewEden.Core;
 using TheGuideToTheNewEden.WinUI.Helpers;
 using TheGuideToTheNewEden.WinUI.Notifications;
 using TheGuideToTheNewEden.WinUI.Services;
-
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 
 namespace TheGuideToTheNewEden.WinUI
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
     public partial class App : Application
     {
         private NotificationManager notificationManager;
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
             this.InitializeComponent();
@@ -30,13 +34,10 @@ namespace TheGuideToTheNewEden.WinUI
             UnhandledException += App_UnhandledException;//UI线程
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;//后台线程
             Log.Init();
-            //ResourceDictionary d = new ResourceDictionary();
-            //Application.Current.Resources.MergedDictionaries[0]
         }
-
         private void CurrentDomain_UnhandledException(object sender, System.UnhandledExceptionEventArgs e)
         {
-            if(e.IsTerminating)
+            if (e.IsTerminating)
             {
                 Log.Error("发生致命错误");
             }
@@ -53,12 +54,6 @@ namespace TheGuideToTheNewEden.WinUI
             notificationManager.Unregister();
             ForegroundWindowService.Current.Stop();
         }
-
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
             Services.ActivationService.Init();
@@ -72,15 +67,6 @@ namespace TheGuideToTheNewEden.WinUI
         }
 
         private Window m_window;
-
-        public static void ToForeground()
-        {
-            //if (m_window != null)
-            //{
-            //    HWND hwnd = (HWND)WinRT.Interop.WindowNative.GetWindowHandle(mainWindow);
-            //    SwitchToThisWindow(hwnd, true);
-            //}
-        }
 
         public static string GetFullPathToExe()
         {
