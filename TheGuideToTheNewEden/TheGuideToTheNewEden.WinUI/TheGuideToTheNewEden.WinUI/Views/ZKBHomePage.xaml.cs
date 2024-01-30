b710df8aa94784fec56819ecdf938610a1106019
QEDSD <2998739882@qq.com>
2024-01-30 10:14:29 +0800

    ZKB搜索优化

--- TheGuideToTheNewEden/TheGuideToTheNewEden.WinUI/TheGuideToTheNewEden.WinUI/Views/ZKBHomePage.xaml.cs
+++ TheGuideToTheNewEden/TheGuideToTheNewEden.WinUI/TheGuideToTheNewEden.WinUI/Views/ZKBHomePage.xaml.cs
@@ -21,6 +21,8 @@ using Windows.Foundation.Collections;
 using TheGuideToTheNewEden.Core.Extensions;
 using System.Xml.Linq;
 using TheGuideToTheNewEden.WinUI.Services;
+using System.Threading;
+using TheGuideToTheNewEden.Core.Models.Universe;
 
 namespace TheGuideToTheNewEden.WinUI.Views
 {
@@ -57,8 +59,12 @@ namespace TheGuideToTheNewEden.WinUI.Views
             VM?.Dispose();
         }
 
+        private CancellationTokenSource _cancellationTokenSource;
         private async void SearchBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
         {
+            _cancellationTokenSource?.Cancel();
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            _cancellationTokenSource = cancellationTokenSource;
             if (_searchItem?.Name == sender.Text)
             {
                 return;
@@ -70,86 +76,115 @@ namespace TheGuideToTheNewEden.WinUI.Views
             else
             {
                 string searchName = sender.Text;
-                sender.ItemsSource =  await Task.Run(() =>
+                var items =  await Task.Run(() =>
                 {
-                    List<IdName> result = new List<IdName>();
-                    var names = Core.Services.IDNameService.SerachByName(searchName);
-                    var typesSearch = Core.Services.DB.InvTypeService.Search(searchName);
-                    List<Core.DBModels.InvType> ships = null;
-                    if(typesSearch.NotNullOrEmpty())
-                    {
-                        var types = Core.Services.DB.InvTypeService.QueryTypes(typesSearch.Select(p=>p.ID).ToList());
-                        var typeGroups = Core.Services.DB.InvGroupService.QueryGroupIdOfCategory(new List<int>()
+                    return Serach(searchName, cancellationTokenSource.Token);
+                });
+                if(!cancellationTokenSource.IsCancellationRequested)
+                {
+                    sender.ItemsSource = items;
+                    Debug.WriteLine($"{sender.Text} : {items.Count}");
+                }
+                else
+                {
+                    Debug.WriteLine("ÒÑÈ¡Ïû");
+                }
+            }
+        }
+        private List<IdName> Serach(string searchName, CancellationToken cancellationToken)
+        {
+            List<IdName> result = new List<IdName>();
+            var names = Core.Services.IDNameService.SerachByName(searchName);
+            if(cancellationToken.IsCancellationRequested)
+            {
+                return null;
+            }
+            var typesSearch = Core.Services.DB.InvTypeService.Search(searchName);
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return null;
+            }
+            List<Core.DBModels.InvType> ships = null;
+            if (typesSearch.NotNullOrEmpty())
+            {
+                var types = Core.Services.DB.InvTypeService.QueryTypes(typesSearch.Select(p => p.ID).ToList());
+                var typeGroups = Core.Services.DB.InvGroupService.QueryGroupIdOfCategory(new List<int>()
                         {
                             3,6,22,23,65,87,
                         });
-                        ships = types.Where(p => typeGroups.Contains(p.GroupID)).ToList();
-                    }
-                    var groups = Core.Services.DB.InvGroupService.QueryGroups(searchName);
-                    var systems = Core.Services.DB.MapSolarSystemService.Search(searchName);
-                    var regions = Core.Services.DB.MapRegionService.Search(searchName);
-                    if(ships.NotNullOrEmpty())
-                    {
-                        foreach(var ship in ships)
-                        {
-                            result.Add(new IdName()
-                            {
-                                Id = ship.TypeID,
-                                Name = ship.TypeName,
-                                Category = (int)IdName.CategoryEnum.InventoryType
-                            });
-                        }
-                    }
-                    if (groups.NotNullOrEmpty())
-                    {
-                        foreach (var group in groups)
-                        {
-                            result.Add(new IdName()
-                            {
-                                Id = group.GroupID,
-                                Name = group.GroupName,
-                                Category = (int)IdName.CategoryEnum.Group
-                            });
-                        }
-                    }
-                    if (systems.NotNullOrEmpty())
-                    {
-                        foreach (var system in systems)
-                        {
-                            result.Add(new IdName()
-                            {
-                                Id = system.ID,
-                                Name = system.Name,
-                                Category = (int)IdName.CategoryEnum.SolarSystem
-                            });
-                        }
-                    }
-                    if (regions.NotNullOrEmpty())
-                    {
-                        foreach (var region in regions)
-                        {
-                            result.Add(new IdName()
-                            {
-                                Id = region.ID,
-                                Name = region.Name,
-                                Category = (int)IdName.CategoryEnum.Region
-                            });
-                        }
-                    }
-                    if (names.NotNullOrEmpty())
-                    {
-                        var hashSet = result.Select(p => p.Id).ToHashSet2();
-                        foreach (var name in names)
-                        {
-                            if(!hashSet.Contains(name.Id))
-                            {
-                                result.Add(name);
-                            }
-                        }
-                    }
-                    return result;
-                });
+                ships = types.Where(p => typeGroups.Contains(p.GroupID)).ToList();
             }
+            var groups = Core.Services.DB.InvGroupService.QueryGroups(searchName);
+            var systems = Core.Services.DB.MapSolarSystemService.Search(searchName);
+            var regions = Core.Services.DB.MapRegionService.Search(searchName);
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return null;
+            }
+            if (ships.NotNullOrEmpty())
+            {
+                foreach (var ship in ships)
+                {
+                    result.Add(new IdName()
+                    {
+                        Id = ship.TypeID,
+                        Name = ship.TypeName,
+                        Category = (int)IdName.CategoryEnum.InventoryType
+                    });
+                }
+            }
+            if (groups.NotNullOrEmpty())
+            {
+                foreach (var group in groups)
+                {
+                    result.Add(new IdName()
+                    {
+                        Id = group.GroupID,
+                        Name = group.GroupName,
+                        Category = (int)IdName.CategoryEnum.Group
+                    });
+                }
+            }
+            if (systems.NotNullOrEmpty())
+            {
+                foreach (var system in systems)
+                {
+                    result.Add(new IdName()
+                    {
+                        Id = system.ID,
+                        Name = system.Name,
+                        Category = (int)IdName.CategoryEnum.SolarSystem
+                    });
+                }
+            }
+            if (regions.NotNullOrEmpty())
+            {
+                foreach (var region in regions)
+                {
+                    result.Add(new IdName()
+                    {
+                        Id = region.ID,
+                        Name = region.Name,
+                        Category = (int)IdName.CategoryEnum.Region
+                    });
+                }
+            }
+            if (names.NotNullOrEmpty())
+            {
+                var hashSet = result.Select(p => p.Id).ToHashSet2();
+                foreach (var name in names)
+                {
+                    if (!hashSet.Contains(name.Id))
+                    {
+                        result.Add(name);
+                    }
+                }
+            }
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return null;
+            }
+            return result;
         }
         private IdName _searchItem;
         private void SearchBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
