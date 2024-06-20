10b90a1f1eb210e136c4b59340e1a989d72f0595
QEDSD <2998739882@qq.com>
2024-06-20 18:03:47 +0800

    星图筛选暂存

---
+++ TheGuideToTheNewEden/TheGuideToTheNewEden.WinUI/TheGuideToTheNewEden.WinUI/Controls/SystemFilterControl.xaml.cs
@@ -0,0 +1,30 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace TheGuideToTheNewEden.WinUI.Controls
+{
+    public sealed partial class SystemFilterControl : UserControl
+    {
+        public SystemFilterControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_SelecteAllRegion_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
