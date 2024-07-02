ad125ff3526b5a3eef09cc3a42cb752079a30a4c
QEDSD <2998739882@qq.com>
2024-07-02 17:58:39 +0800

    星图导航两点最近距离及路径

---
+++ TheGuideToTheNewEden/TheGuideToTheNewEden.Core/EVEHelpers/Dijkstras.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheGuideToTheNewEden.Core.EVEHelpers
+{
+    internal class Dijkstras
+    {
+        readonly Dictionary<int, Dictionary<int, int>> _vertices = new Dictionary<int, Dictionary<int, int>>();
+
+        public void AddVertex(int name, Dictionary<int, int> edges)
+        {
+            _vertices[name] = edges;
+        }
+        public void RemoveVertex(int name)
+        {
+            _vertices.Remove(name);
+        }
+
+        public List<int> CalShortestPath(int start, int end)
+        {
+            var previous = new Dictionary<int, int>();
+            var distances = new Dictionary<int, int>();
+            var nodes = new List<int>();
+
+            List<int> path = null;
+
+            foreach (var vertex in _vertices)
+            {
+                if (vertex.Key == start)
+                {
+                    distances[vertex.Key] = 0;
+                }
+                else
+                {
+                    distances[vertex.Key] = int.MaxValue;
+                }
+
+                nodes.Add(vertex.Key);
+            }
+
+            while (nodes.Count != 0)
+            {
+                nodes.Sort((x, y) => distances[x] - distances[y]);
+
+                var smallest = nodes[0];
+                nodes.Remove(smallest);
+
+                if (smallest == end)
+                {
+                    path = new List<int>();
+                    while (previous.ContainsKey(smallest))
+                    {
+                        path.Add(smallest);
+                        smallest = previous[smallest];
+                    }
+
+                    break;
+                }
+
+                if (distances[smallest] == int.MaxValue)
+                {
+                    break;
+                }
+
+                foreach (var neighbor in _vertices[smallest])
+                {
+                    var alt = distances[smallest] + neighbor.Value;
+                    if (alt < distances[neighbor.Key])
+                    {
+                        distances[neighbor.Key] = alt;
+                        previous[neighbor.Key] = smallest;
+                    }
+                }
+            }
+
+            return path;
+        }
+    }
+}
---
+++ TheGuideToTheNewEden/TheGuideToTheNewEden.Core/EVEHelpers/ShortestPathHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheGuideToTheNewEden.Core.Extensions;
+
+namespace TheGuideToTheNewEden.Core.EVEHelpers
+{
+    public class ShortestPathHelper
+    {
+        private readonly Dijkstras _dijkstras = new Dijkstras();
+        public ShortestPathHelper()
+        {
+            var ps = SolarSystemPosHelper.PositionDic;
+            foreach(var p in ps.Values)
+            {
+                if (p.JumpTo.NotNullOrEmpty())
+                {
+                    Dictionary<int, int> edges = new Dictionary<int, int>();
+                    foreach (var jump in p.JumpTo)
+                    {
+                        edges.Add(jump, 1);
+                    }
+                    _dijkstras.AddVertex(p.SolarSystemID, edges);
+                }
+            }
+        }
+        public void AddAvoid(int id)
+        {
+            _dijkstras.RemoveVertex(id);
+        }
+        public void AddAvoid(List<int> ids)
+        {
+            foreach(var id in ids)
+            {
+                AddAvoid(id);
+            }
+        }
+        public List<int> Cal(int start, int end)
+        {
+            return _dijkstras.CalShortestPath(start, end);
+        }
+    }
+}
