9877ea3898c7bf8e1f3d5112a8024184bd3264ab
QEDSD <2998739882@qq.com>
2024-07-18 17:24:28 +0800

    星图屏蔽地理信息统计工具

--- TheGuideToTheNewEden/TheGuideToTheNewEden.WinUI/TheGuideToTheNewEden.WinUI/Views/Map/MapPage.xaml.cs
+++ TheGuideToTheNewEden/TheGuideToTheNewEden.WinUI/TheGuideToTheNewEden.WinUI/Views/Map/MapPage.xaml.cs
@@ -89,7 +89,6 @@ namespace TheGuideToTheNewEden.WinUI.Views.Map
             await InitStatistics();
             MapNavigation.SetData(MapCanvas,_systemKills, _systemJumps, _sovDatas);
             OneJumpCover.SetData(MapCanvas, _systemKills, _systemJumps, _sovDatas);
-            MapGIS.SetData(_mapSolarSystems, _mapRegions, MapCanvas, _systemKills, _systemJumps, _sovDatas);
             _window?.HideWaiting();
         }
         private async Task InitData()
@@ -603,7 +602,6 @@ namespace TheGuideToTheNewEden.WinUI.Views.Map
                 case "PlanetResource_Upgrade": targetTool = UpgradeList; break;
                 case "OneJumpCover": targetTool = Tool_OneJumpCover; break;
                 case "Navigation": targetTool = Tool_Navigation; break;
-                case "GIS": targetTool = Tool_GIS; break;
             }
             if(targetTool == null)
             {
