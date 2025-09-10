26ba65fb3cd7b1f776c6b8d3e605337840d79079
QEDSD <qedsd@qq.com>
2025-09-10 14:42:40 +0800

    切换至新图片服务器

--- TheGuideToTheNewEden/TheGuideToTheNewEden.WinUI/TheGuideToTheNewEden.WinUI/Converters/GameImageConverter.cs
+++ TheGuideToTheNewEden/TheGuideToTheNewEden.WinUI/TheGuideToTheNewEden.WinUI/Converters/GameImageConverter.cs
@@ -1,15 +1,21 @@
-﻿using Microsoft.UI.Xaml.Data;
+﻿using ESI.NET.Models.Character;
+using Microsoft.UI.Xaml.Data;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Buffers.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Vanara.PInvoke.Kernel32;
 
 namespace TheGuideToTheNewEden.WinUI.Converters
 {
     public sealed class GameImageConverter : IValueConverter
     {
+        private const string TranquilityURL = "https://images.evetech.net";
+        private const string Serenity = "https://image.evepc.163.com";
+
         public int Size { get; set; } = 128;
         public ImgType Type { get; set; } = ImgType.Type;
         public object Convert(object value, Type targetType, object parameter, string language)
@@ -27,9 +33,32 @@ namespace TheGuideToTheNewEden.WinUI.Converters
                 return null;
             switch (Services.GameServerSelectorService.Value)
             {
-                case Core.Enums.GameServerType.Tranquility: return $"https://imageserver.eveonline.com/{type}/{id}_{size}.{GetImageFormat(type)}";
-                case Core.Enums.GameServerType.Serenity: return $"https://image.evepc.163.com/{type}/{id}_{size}.{GetImageFormat(type)}";
+                case Core.Enums.GameServerType.Tranquility: return GetTranquilityImage(id, type, size);
+                case Core.Enums.GameServerType.Serenity: return $"{Serenity}/{type}/{id}_{size}.{GetImageFormat(type)}";
                 default: return string.Empty;
+            }
+        }
+        private static string GetTranquilityImage(int id, ImgType type, int size = 128)
+        {
+            switch (type)
+            {
+                case ImgType.Alliance: return $"{TranquilityURL}/alliances/{id}/logo?size={size}";
+                case ImgType.Corporation: return $"{TranquilityURL}/corporations/{id}/logo?size={size}";
+                case ImgType.Character: return $"{TranquilityURL}/characters/{id}/portrait?size={size}";
+                case ImgType.Type:
+                    {
+                        int rootMarketGroup = Core.Services.DB.InvMarketGroupService.QueryRootGroupOfType(id);
+                        bool isRenderType = rootMarketGroup == 4 | rootMarketGroup == 157;//舰船or无人机
+                        if (isRenderType)
+                        {
+                            return $"{TranquilityURL}/types/{id}/render?size={size}";
+                        }
+                        else
+                        {
+                            return $"{TranquilityURL}/types/{id}/icon?size={size}";
+                        }
+                    }
+                    default: return string.Empty;
             }
         }
         private static string GetImageFormat(ImgType type)
