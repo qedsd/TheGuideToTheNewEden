db546a193aefe17239f803b31d17475c113b1a04
QEDSD <qedsd@qq.com>
2026-02-03 11:37:10 +0800

    星图预警工具暂存

---
+++ TheGuideToTheNewEden/TheGuideToTheNewEden.WinUI/TheGuideToTheNewEden.WinUI/Views/Map/Tools/IntelTool.xaml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace TheGuideToTheNewEden.WinUI.Views.Map.Tools
+{
+    public sealed partial class IntelTool : Page
+    {
+        public IntelTool()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
